1ce9e63be7c6ef6a1ac01b92341628d49f6f36f1
Ella Hoeppner <ellahoeppner@gmail.com>
2026-06-13 17:13:39 -0400

    adding top-level lib exports

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,11 @@ pub mod window;
 #[derive(Debug)]
 pub(crate) enum Never {}
 
+pub use compiler::core::compile_easl_file_to_target;
 pub use compiler::core::compile_easl_file_to_wgsl;
+pub use compiler::core::compile_easl_source_to_target;
 pub use compiler::core::compile_easl_source_to_wgsl;
 pub use compiler::core::get_easl_program_info;
 pub use compiler::core::load_easl_program_from_file;
+pub use compiler::program::CompilerTarget;
 pub use format::format_easl_source;
